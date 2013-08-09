c969cb32b174c6d90ea51e59371dc7eea9e1c995
Kaiyang.tee <kaiyang.tee@wustl.edu>
2013-08-09 14:24:09 +0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Staticspages::Application.config.secret_token = '7b18452efcca7bec6f5bfc463ae4fb0a21a995a0493945e8ba2e3857b694a330b7eae4556dcaf7202ec2283371ac446643805ac6d6a761ba4bba48efde246806'
