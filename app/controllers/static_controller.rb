df7066cd53fbd20cb05c25bb8e76280c8e3c8945
Kaiyang.tee <kaiyang.tee@wustl.edu>
2013-08-10 12:37:31 +0800

    All text added in, basics complete

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,4 +2,16 @@ class StaticController < ApplicationController
   def index
 
   end
+
+  def find_us
+
+  end
+
+  def massage_treatment
+
+  end
+  
+  def body
+    
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Staticspages::Application.routes.draw do
  
  root to: "static#index"
  get "/find_us", to: "static#find_us"
+ get "/massage_treatment", to: "static#massage_treatment"
+ get "/facial", to: "static#facial"
+ get "/body", to: "static#body"
 end
